288b8180a557a8e83fb1409e7cbb5a48a82fdc1f
Honey10-code <hanipathak8104@gmail.com>
2026-04-26 10:02:41 +0530

    chore: remove hardcoded MongoDB URI secret

--- server/verify_axn.js
+++ server/verify_axn.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // The one that worked before
-const AXN_URI = "mongodb://hanipathak8104_db_user:TYq3NlwRbsq8YxpJ@ac-3r8tlx-shard-00-00.axxnaha.mongodb.net:27017,ac-3r8tlx-shard-00-01.axxnaha.mongodb.net:27017,ac-3r8tlx-shard-00-02.axxnaha.mongodb.net:27017/rkcare?ssl=true&replicaSet=atlas-10akii-shard-0&authSource=admin&retryWrites=true&w=majority";
-
+require('dotenv').config();
+const AXN_URI = process.env.MONGO_URI;
 console.log("Testing AXN Cluster...");
 
 mongoose.connect(AXN_URI)
